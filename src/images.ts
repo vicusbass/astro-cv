7eb4dd6d5282f4eeca9148ccf18ff0700e1a860c
Vasile Pop <vasile.pop@gmail.com>
2025-07-11 15:27:10 +0300

    add nosto

--- src/images.ts
+++ src/images.ts
@@ -2,10 +2,12 @@ import profileImage from './assets/profile.png';
 import healthAcademyLogo from './assets/HealthAcademyLogo.png';
 import butAScratch from './assets/but-a-scratch.jpeg';
 import whale from './assets/whale.svg';
+import nosto from './assets/nosto.svg';
 
 export {
   profileImage,
   healthAcademyLogo,
   butAScratch,
-  whale
+  whale,
+  nosto
 };
