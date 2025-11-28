68516479644d3add7bdcf5ee5710e6c2f6319ec2
Vasile Pop <vasile.pop@gmail.com>
2025-11-28 11:14:06 +0200

    new project

--- src/images.ts
+++ src/images.ts
@@ -3,11 +3,13 @@ import healthAcademyLogo from './assets/HealthAcademyLogo.png';
 import butAScratch from './assets/but-a-scratch.jpeg';
 import whale from './assets/whale.svg';
 import nosto from './assets/nosto.svg';
+import symbold from './assets/symbold.png';
 
 export {
   profileImage,
   healthAcademyLogo,
   butAScratch,
   whale,
-  nosto
+  nosto,
+  symbold
 };
