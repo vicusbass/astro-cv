d376957825e93e00c9c22e61b33daa97a30e61c1
Vasile Pop <vasile.pop@gmail.com>
2025-11-28 11:25:26 +0200

    add og image

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'Vasile Pop CV - Made with Astro'
-export const SITE_DESCRIPTION = 'SDET, QA Engineer, Dad, Husband and Musician'
+export const SITE_DESCRIPTION = 'SDET, QA Engineer, Web Developer, Dad, Husband and Musician'
+export const SITE_NAME = 'Vasile Pop'
+export const SITE_URL = 'https://vicusbass.com'
